3f6e2399a088ec2fafef4396193428cb8510879b
anydes <jashondagoat22@gmail.com>
2026-05-11 17:47:23 -0400

    Update u.lua

--- u.lua
+++ u.lua
@@ -216,9 +216,9 @@ local spotQueueActive = false
 local config = {
     sacrifice = SACRIFICE_NAME,
     farmer = FARMER_NAME,
-    scoreMethod = "dunk", -- "shoot" or "dunk"
+    scoreMethod = "shoot", -- "shoot" or "dunk"
     endMethod = "score", -- "score" = 21-0/22-0 trigger, "shots" = old shotsToTake trigger
-    defesnebadges = false, -- if true: override ankle/shoot/dunk logic with defense-badge behavior
+    defesnebadges = true, -- if true: override ankle/shoot/dunk logic with defense-badge behavior
     blocksToGet = 15, -- used only when defesnebadges=true (SACRIFICE teleports after this many blocks)
     tweenSpeed = 1.5, -- default tween time for general movement tweens
     movementCheck = 0.05,
@@ -226,7 +226,7 @@ local config = {
     shootDist = 12,
     shootDistTolerance = 1, -- allowed +/- studs from shootDist before retrying
     followDist = 4.5,
-    anklesToShoot = 1,
+    anklesToShoot = 13,
     shotsToTake = 11,
     shootTweenTime = 0.7,
     spotCoords = Vector3.new(486, 85, 111),
